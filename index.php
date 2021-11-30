e2d6f18df84f707dc7606ecf0df3ff3a6d57a8e5
4mirul1999 <90590041+4mirul1999@users.noreply.github.com>
2021-11-30 23:45:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+  $my_string = "Hello Guys, I am Amirul Aiman Bin Noor Nasruddin, the Cloud Computer expert!. Please go to my GitHub accoun https://github.com/4mirul1999 "; 
+  echo $my_string;
+?>
